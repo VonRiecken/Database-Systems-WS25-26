24af569f9fd7c2ac07484172002ec9800264b146
sulthanmuhammed <sulthan2510@gmail.com>
2025-11-26 11:45:54 +0100

    Initial commit

--- database.py
+++ database.py
@@ -1,13 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import text
+import configparser
 
-# UPDATE 'password' with your actual Postgres password
-# DATABASE_URL = "postgresql://postgres:123ugofree@localhost/UniversityCanteen"
-# DATABASE_URL = "postgresql://postgres:123ugofree@localhost/burgerproject"
-DATABASE_URL = "postgresql://postgres:123ugofree@localhost/postgres"
+config = configparser.ConfigParser()
+config.read('config/config.ini')
+db_config = config['postgres']
 
+DATABASE_URL = f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}/{db_config['database']}"
 engine = create_engine(DATABASE_URL)
+
+# db_config = config['mysql']
+# DATABASE_URL = "mysql+pymysql://root:xxxxxxxxxxxx@reddata.m.hs-offenburg.de:3306/dbsysgr1"
+# DATABASE_URL = f"mysql+pymysql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# engine = create_engine(
+#     DATABASE_URL, 
+#     # Optional but recommended for MySQL to handle encoding correctly:
+#     connect_args={"charset": "utf8mb4"},
+#     # Optional: MySQL closes connections after 8 hours. 
+#     # This prevents "MySQL server has gone away" errors by recycling connections every hour.
+#     pool_recycle=3600
+# )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
@@ -16,4 +30,5 @@ def get_db():
         yield db
     finally:
         db.close()
+
 
